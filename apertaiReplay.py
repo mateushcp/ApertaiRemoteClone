d94c5b83a28beecc6b5647e54d4facd6621b40d3
Mateus Coelho <56733190+mateushcp@users.noreply.github.com>
2024-07-11 17:06:02 -0300

    Update apertaiReplay.py

--- apertaiReplay.py
+++ apertaiReplay.py
@@ -54,7 +54,14 @@ def start_buffer_streams():
         process2, buffer_file_pattern2 = start_buffer_stream(url, cam_id, 2)
         buffers[cam_id]['buffer2'] = (process2, buffer_file_pattern2)
 
+    # Verificar se todos os buffers foram criados
+    for cam_id in cameras.keys():
+        for buffer_number in [1, 2]:
+            if not get_latest_buffer_file(cam_id, buffer_number):
+                print(f"Error: Buffer {buffer_number} for {cam_id} was not created correctly.")
+                return False
     print("Buffers have been created. The program is ready to run.")
+    return True
 
 def get_latest_buffer_file(cam_id, buffer_number):
     buffer_file_pattern = f'{cam_id}_buffer{buffer_number}'
@@ -106,7 +113,9 @@ def main():
     for i, ip in enumerate(rtsp_devices):
         cameras[f"cam{i+1}"] = f"rtsp://apertaiCam{i+1}:130355va@{ip}/stream1"
 
-    start_buffer_streams()  # Inicia a gravação
+    if not start_buffer_streams():  # Inicia a gravação e verifica se todos os buffers foram criados
+        print("Error: Not all buffers were created successfully.")
+        return
 
     while True:
         for button_id, button in buttons.items():
